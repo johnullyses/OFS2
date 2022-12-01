9ae90db4ee6032b4f4ae5cc170dacd0805e94e2a
John Ullyses Capellan <john.capellan@mdscsi.com>
2022-12-01 15:59:13 +0800

    test2

--- config/envparser.js
+++ config/envparser.js
@@ -1,12 +1,26 @@
+// const DotEnv = require('dotenv')
+
+// module.exports = function (ctx) {
+//   // Let's stringify our variables
+//   const parsedEnv = DotEnv.config({ path: ctx.dev ? '.env' : '.env.prod' }).parsed
+
+//   for (key in parsedEnv) {
+//     if (typeof parsedEnv[key] === 'string') {
+//       parsedEnv[key] = parsedEnv[key]
+//     }
+//   }
+//   return parsedEnv
+// }
+
+// config/envparser.js
 const DotEnv = require('dotenv')
+const parsedEnv = DotEnv.config().parsed
 
-module.exports = function (ctx) {
+module.exports = function () {
   // Let's stringify our variables
-  const parsedEnv = DotEnv.config({ path: ctx.dev ? '.env' : '.env.prod' }).parsed
-
   for (key in parsedEnv) {
     if (typeof parsedEnv[key] === 'string') {
-      parsedEnv[key] = parsedEnv[key]
+      parsedEnv[key] = JSON.stringify(parsedEnv[key])
     }
   }
   return parsedEnv
