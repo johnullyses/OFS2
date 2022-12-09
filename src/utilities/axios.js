bde0d2291273197749aa0b2706a738c8f2e560f9
John Ullyses Capellan <john.capellan@mdscsi.com>
2022-12-09 15:38:12 +0800

    test7

--- src/utilities/axios.js
+++ src/utilities/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 
 const axiosInstance = axios.create({
-  baseURL: 'http://15.235.86.171:10005/ofs_api/public/index.php/api/login',
+  baseURL: 'http://15.235.86.171:10005/ofs_api/public/index.php',
   timeout: 300000,
   headers: {
     'Accept': 'application/json',
